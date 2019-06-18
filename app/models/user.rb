4a2c2959ed118af28c5126d501bf0c565186d0bf
luigi manganiello <luigi.manga92@gmail.com>
2019-06-18 12:19:09 +0200

    validations completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :shops
   has_many :cards
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
