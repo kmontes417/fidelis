b92f96cfe613322b9dbd12b2bd7ef7ac68339178
luigi manganiello <luigi.manga92@gmail.com>
2019-06-18 12:57:56 +0200

    added show & index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_100617) do
+ActiveRecord::Schema.define(version: 2019_06_18_104631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_100617) do
     t.integer "max_stars", default: 10
     t.string "address"
     t.string "photo"
+    t.string "category"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
