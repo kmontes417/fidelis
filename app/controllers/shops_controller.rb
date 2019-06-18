3441ba7aebebbdf3b1310a1bbe4de9f59e60f17e
pietro ventimiglia <twntymls@gmail.com>
2019-06-18 14:59:42 +0200

    resolved conflict

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,7 @@
 class ShopsController < ApplicationController
-<<<<<<< HEAD
   def index
+
+    @shop = Shop.All
     @shops = Shop.where.not(latitude: nil, longitude: nil)
 
     @markers = @shops.map do |shop|
@@ -10,16 +11,9 @@ class ShopsController < ApplicationController
         infoWindow: render_to_string(partial: "infowindow", locals: { shop: shop })
       }
     end
-=======
+
   def show
     @shop = Shop.find params[:id]
   end
 
-  def index
-    # if params["search"].present?
-    # else
-      @shop = Shop.All
-    # end
->>>>>>> master
-  end
 end
