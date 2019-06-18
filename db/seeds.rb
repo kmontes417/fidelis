1a0b9117de5829a5ddfd17112ca120a22dd9ae62
Richard Haenel <richardhaenel1@gmail.com>
2019-06-18 12:11:48 +0200

    fixed user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,10 @@ puts  "Cleaned DB"
 puts "Creating User seeds"
 
 USERS = [
-User.create!(first_name: "Alessio", last_name: "Del Piero", remote_photo_url: "https://media.licdn.com/dms/image/C4E0BAQEi1tZolS3h9g/company-logo_200_200/0?e=2159024400&v=beta&t=IoV7oTesJjHHglA8fEsjFBSjeGCTIqhVpgQkAB_mJao", email: "richard@lewagon.com", password: "123456", host: true),
-User.create!(first_name: "Luigi", last_name: "Buffon", remote_photo_url: "https://media.licdn.com/dms/image/C4E0BAQEi1tZolS3h9g/company-logo_200_200/0?e=2159024400&v=beta&t=IoV7oTesJjHHglA8fEsjFBSjeGCTIqhVpgQkAB_mJao", email: "richard@lewagon.com", password: "123456", host: true),
-User.create!(first_name: "Pietro", last_name: "Czech", remote_photo_url: "https://media.licdn.com/dms/image/C4E0BAQEi1tZolS3h9g/company-logo_200_200/0?e=2159024400&v=beta&t=IoV7oTesJjHHglA8fEsjFBSjeGCTIqhVpgQkAB_mJao", email: "richard@lewagon.com", password: "123456", host: true),
-User.create!(first_name: "Krystal", last_name: "Montolivo", remote_photo_url: "https://media.licdn.com/dms/image/C4E0BAQEi1tZolS3h9g/company-logo_200_200/0?e=2159024400&v=beta&t=IoV7oTesJjHHglA8fEsjFBSjeGCTIqhVpgQkAB_mJao", email: "richard@lewagon.com", password: "123456", host: true),
-User.create!(first_name: "Richard", last_name: "Totti", remote_photo_url: "https://media.licdn.com/dms/image/C4E0BAQEi1tZolS3h9g/company-logo_200_200/0?e=2159024400&v=beta&t=IoV7oTesJjHHglA8fEsjFBSjeGCTIqhVpgQkAB_mJao", email: "richard@lewagon.com", password: "123456", host: true),
+User.create!(first_name: "Alessio", last_name: "Del Piero", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/babbage93", email: "alessio@lewagon.com", password: "123456", host: true),
+User.create!(first_name: "Luigi", last_name: "Buffon", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/dub-G", email: "luigi@lewagon.com", password: "123456", host: true),
+User.create!(first_name: "Pietro", last_name: "Czech", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/twentymls", email: "pietro@lewagon.com", password: "123456", host: true),
+User.create!(first_name: "Krystal", last_name: "Montolivo", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/kmontes417", email: "krystal@lewagon.com", password: "123456", host: true),
+User.create!(first_name: "Richard", last_name: "Totti", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/richardhaenel", email: "richard@lewagon.com", password: "123456", host: true),
 ]
 
