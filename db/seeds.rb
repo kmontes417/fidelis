5dbd22d2e7cb730248cf12e53a97b441661b23c8
alessio <albez93@gmail.com>
2019-06-18 15:24:16 +0200

    qr column added

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,35 +8,27 @@ puts  "Cleaned DB"
 
 
 OWNERS = [
-User.create!(first_name: "Lucia", last_name: "Ferrara", remote_photo_url: "https://randomuser.me/api/portraits/women/43.jpg", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Alessandro", last_name: "Villa", remote_photo_url: "https://randomuser.me/api/portraits/men/22.jpg", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Giuseppe", last_name: "Colombo", remote_photo_url: "https://randomuser.me/api/portraits/men/47.jpg", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Sofia", last_name: "Bianchi", remote_photo_url: "https://randomuser.me/api/portraits/women/10.jpg", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Agnese", last_name: "Costa", remote_photo_url: "https://randomuser.me/api/portraits/women/62.jpg", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Salvatore", last_name: "Esposito", remote_photo_url: "https://randomuser.me/api/portraits/men/4.jpg", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Federica", last_name: "Rossi", remote_photo_url: "https://randomuser.me/api/portraits/women/79.jpg", email: "alessio@lewagon.com", password: "123456"),
+User.create!(first_name: "Lucia", last_name: "Ferrara", photo: "https://randomuser.me/api/portraits/women/43.jpg", email: "lucia@lewagon.com", password: "123456"),
+User.create!(first_name: "Alessandro", last_name: "Villa", photo: "https://randomuser.me/api/portraits/men/22.jpg", email: "alessandro@lewagon.com", password: "123456"),
+User.create!(first_name: "Giuseppe", last_name: "Colombo", photo: "https://randomuser.me/api/portraits/men/47.jpg", email: "giuseppe@lewagon.com", password: "123456"),
+User.create!(first_name: "Sofia", last_name: "Bianchi", photo: "https://randomuser.me/api/portraits/women/10.jpg", email: "sofia@lewagon.com", password: "123456"),
+User.create!(first_name: "Agnese", last_name: "Costa", photo: "https://randomuser.me/api/portraits/women/62.jpg", email: "agnese@lewagon.com", password: "123456"),
+User.create!(first_name: "Salvatore", last_name: "Esposito", photo: "https://randomuser.me/api/portraits/men/4.jpg", email: "salvatore@lewagon.com", password: "123456"),
+User.create!(first_name: "Federica", last_name: "Rossi", photo: "https://randomuser.me/api/portraits/women/79.jpg", email: "federica@lewagon.com", password: "123456"),
 ]
 
 puts "Creating Shops"
 
 
-Shop.create!(name:"Gelateria Duomo", description:"Best gelato in Milan", max_stars:10, user: OWNERS.sample, photo: "https://unsplash.com/photos/Xpt4_HgZSjw", category: "gelateria", address: "Via Giovanni Boccaccio, 9, Milan")
-Shop.create!(name:"Gelateria da Mario", description:"Artisanal gelato ", max_stars:10, user: OWNERS.sample, photo: "https://unsplash.com/photos/alEZLDPPRBU", category: "gelateria", address:"Via Mercato, 20, Milan")
-Shop.create!(name:"Pizzeria da Luca", description:"Authentic Pizza from Naple", max_stars:10, user: OWNERS.sample, photo: "https://unsplash.com/photos/22Vt7JIf7ZI", category: "pizzeria", address:"Via Edmondo de Amicis, 22, Milan")
-Shop.create!(name:"Gelateria Ambrogio", description:"Gelato made with love", max_stars:10, user: OWNERS.sample, photo: "https://unsplash.com/photos/8beTH4VkhLI", category: "gelateria", address:"Via Spadari, 13, Milan")
-Shop.create!(name:"Osteria Sora Loci", description:"Best local cuisine", max_stars:10, user: OWNERS.sample, photo: "https://unsplash.com/photos/AvLo-ZG6fVY", category: "osteria", address:"Via Tortona, 3, Milan")
-Shop.create!(name:"Pizzeria Baggio", description:"Great Price-Quality", max_stars:10, user: OWNERS.sample, photo: "https://unsplash.com/photos/IODFFLZyDlI", category: "pizzeria", address:"Via Giovanni Gherardini, Milan")
+Shop.create!(name:"Gelateria Duomo", description:"Best gelato in Milan", user: OWNERS.sample, photo: "https://unsplash.com/photos/Xpt4_HgZSjw", category: "gelateria", address: "Via Giovanni Boccaccio, 9, Milan")
+Shop.create!(name:"Gelateria da Mario", description:"Artisanal gelato ", user: OWNERS.sample, photo: "https://unsplash.com/photos/alEZLDPPRBU", category: "gelateria", address:"Via Mercato, 20, Milan")
+Shop.create!(name:"Pizzeria da Luca", description:"Authentic Pizza from Naple", user: OWNERS.sample, photo: "https://unsplash.com/photos/22Vt7JIf7ZI", category: "pizzeria", address:"Via Edmondo de Amicis, 22, Milan")
+Shop.create!(name:"Gelateria Ambrogio", description:"Gelato made with love", user: OWNERS.sample, photo: "https://unsplash.com/photos/8beTH4VkhLI", category: "gelateria", address:"Via Spadari, 13, Milan")
+Shop.create!(name:"Osteria Sora Loci", description:"Best local cuisine", user: OWNERS.sample, photo: "https://unsplash.com/photos/AvLo-ZG6fVY", category: "osteria", address:"Via Tortona, 3, Milan")
+Shop.create!(name:"Pizzeria Baggio", description:"Great Price-Quality", user: OWNERS.sample, photo: "https://unsplash.com/photos/IODFFLZyDlI", category: "pizzeria", address:"Via Giovanni Gherardini, Milan")
 
 
-puts "Creating User seeds"
 
-USERS = [
-User.create!(first_name: "Alessio", last_name: "Del Piero", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/babbage93", email: "alessio@lewagon.com", password: "123456"),
-User.create!(first_name: "Luigi", last_name: "Buffon", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/dub-G", email: "luigi@lewagon.com", password: "123456"),
-User.create!(first_name: "Pietro", last_name: "Marini", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/twentymls", email: "pietro@lewagon.com", password: "123456"),
-User.create!(first_name: "Krystal", last_name: "Montolivo", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/kmontes417", email: "krystal@lewagon.com", password: "123456"),
-User.create!(first_name: "Richard", last_name: "Totti", remote_photo_url: "https://kitt.lewagon.com/placeholder/users/richardhaenel", email: "richard@lewagon.com", password: "123456"),
-]
 
 
 puts "Creating Cards"
