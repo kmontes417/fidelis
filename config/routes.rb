9314179d497ca06ca99600d268e99117dd8d0265
Krystal Montesdeoca <47454203+kmontes417@users.noreply.github.com>
2019-06-19 11:22:08 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :shops, only: [:index, :show] do
     resources :promotion
-<<<<<<< HEAD
     resources :cards, only: [:new, :create, :show, :edit, :update, :destroy]
     get '/qr/:id', to: 'pages#add_stamp'
-=======
-    resources :cards, only: [:create, :show, :update, :destroy]
->>>>>>> master
   end
   get '/dashboard', to: 'pages#dashboard'
 end
