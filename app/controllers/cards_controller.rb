ad37fa08d3131b2b16d2ff840bd6236daf292feb
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-19 13:14:59 +0200

    updated card_controller methods

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,35 +1,14 @@
 class CardsController < ApplicationController
-  def show
-  end
-
   def create
-    @user = User.find(params[:user_id])
-    @shop = Shop.find(current_user.shop_id)
-    @card = Card.new(@user, @shop)
-    @card.save
-    redirect_to root_path
   end
 
   def update
-    @user = User.find(params[:user_id])
-    @shop = Shop.find(current_user.shop_id)
-    @card = Card.find(@user, @shop)
-
-    @card.update(star_count[params[:star_count] + @card.star_count])
-    redirect_to root_path
   end
 
   def destroy
-    @card = Card.find params[:id]
+    @card = Card.find(params[:id])
     @card.destroy
     redirect_to root_path
-  end
-
-  def complete
-    @card = Card.find(params[:id])
-    if @card.status == "completed"
-      render :new
-    end
   end
 
   private
