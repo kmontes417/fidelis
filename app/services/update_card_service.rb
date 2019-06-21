5a108fc78f3f9becc726c5129e47c31d725554e0
luigi manganiello <luigi.manga92@gmail.com>
2019-06-21 11:29:07 +0200

    added fonts and colors

--- app/services/update_card_service.rb
+++ app/services/update_card_service.rb
@@ -3,6 +3,7 @@ class UpdateCardService
     @user_id = user_id
     @shop_id = shop_id
     @star_count = star_count.to_i
+    @counter = 0
   end
 
   def call
@@ -22,15 +23,15 @@ class UpdateCardService
   end
 
   def update_card
-    counter = 0
+    @counter = 0
     @card = Card.where(user_id: @user_id, shop_id: @shop_id, status: "pending").first
     @star_count.to_i.times do
       if @card.star_count < Shop.find(@shop_id).max_stars
         @card.update(star_count: @card.star_count + 1)
         @card.update(status: "completed") if @card.star_count == Shop.find(@shop_id).max_stars
-        counter += 1
+        @counter += 1
       else
-        create_new_card(@star_count - counter)
+        create_new_card(@star_count - @counter)
       end
     end
   end
