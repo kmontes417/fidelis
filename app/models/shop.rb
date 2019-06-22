2e7ff6dd0c7e9c6b0dda6dd77b83d981bef4a486
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-22 11:30:40 +0200

    fixed the edit form for shop info.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,8 +3,11 @@ class Shop < ApplicationRecord
   belongs_to :category
   has_many :promotions
   has_many :cards
+
   validates :name, presence: true
   validates :description, presence: true
+  validates :address, presence: true
   geocoded_by :address
+
   after_validation :geocode, if: :will_save_change_to_address?
 end
