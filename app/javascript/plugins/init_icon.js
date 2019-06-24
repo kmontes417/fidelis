3aa1e73ab6194c4f66868f4aeae75ce18d9241be
pietro ventimiglia <twntymls@gmail.com>
2019-06-24 12:44:33 +0200

    icon logic done

--- app/javascript/plugins/init_icon.js
+++ app/javascript/plugins/init_icon.js
@@ -2,9 +2,9 @@
 
 const initIcon = () => {
 
-  const icon = document.querySelectorAll('.icon');
+  const icons = document.querySelectorAll('.icon');
 
-  icon.forEach(img => {
+  icons.forEach(img => {
     img.addEventListener("click", (event) => {
     if(img.className === (".icon")) {
       img.className = (".icon-onclick");
