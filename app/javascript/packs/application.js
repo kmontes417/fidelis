41e515570eff977c702b42401132466a2213c3e3
alessio <albez93@gmail.com>
2019-06-25 10:13:51 +0200

    Francesco's work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,20 +7,38 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 
 
+const refreshBody = () => {
+
+    Rails.ajax({
+    url: "/dashboard",
+    type: "get",
+    dataType: "html",
+    success: function(data) {
+      console.log(data)
+      const newCards = data.getElementById("cards-ajax");
+      const currentCards = document.getElementById("cards-ajax");
+      currentCards.innerHTML = newCards.outerHTML;
+
+},
+  error: function(data) {
+    console.log("AJAX call did not work!" + data)
+  }
+})
+}
 
 
 document.addEventListener('turbolinks:load', (event) => {
-    console.log('Turbolinks loaded');
-    initMapbox();
+  console.log('Turbolinks loaded');
+  initMapbox();
+
+  const closingModal = document.getElementById("closing-modal");
+  closingModal.addEventListener('click', () => {
+    console.log("yes I am clicking");
+    refreshBody();
+ });
 });
 
 
 
-import { initIcon } from '../plugins/init_icon';
 
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-
-
-initMapbox();
-initIcon();
 
