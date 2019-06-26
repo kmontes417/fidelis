6ccaa33e62153bde4e5281ed083751ff4ad8511e
luigi manganiello <luigi.manga92@gmail.com>
2019-06-26 12:25:05 +0200

    fixed refresh body and small errors

--- app/javascript/plugins/init_refreshBody.js
+++ app/javascript/plugins/init_refreshBody.js
@@ -1,11 +1,14 @@
 const initRefreshBody = () => {
 
+  const modalBtn = document.getElementById("closing-modal");
+  if (!modalBtn) return;
+  modalBtn.addEventListener('click', () => {
     Rails.ajax({
     url: "/dashboard",
     type: "get",
     dataType: "html",
     success: function(data) {
-      console.log(data)
+      console.log("refreshing cards")
       const newCards = data.getElementById("cards-ajax");
       const currentCards = document.getElementById("cards-ajax");
       currentCards.innerHTML = newCards.outerHTML;
@@ -14,6 +17,7 @@ const initRefreshBody = () => {
   error: function(data) {
     console.log("AJAX call did not work!" + data)
   }
-})
+});
+});
 }
 export { initRefreshBody };
